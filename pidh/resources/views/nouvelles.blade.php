e600fb734c3b3073bb04d4d212435247b800a575
Giovanni <giovannitempestainfo@gmail.com>
2022-04-11 13:28:13 +0200

    fix css bugs

--- pidh/resources/views/nouvelles.blade.php
+++ pidh/resources/views/nouvelles.blade.php
@@ -10,8 +10,8 @@
     <title>Nouvelles du monde</title>
     <link rel="stylesheet" href="{{ URL::asset('css/nicepage.css') }}" media="screen">
     <link rel="stylesheet" href="{{ URL::asset('css/Nouvelles-du-monde.css') }}" media="screen">
-    <script class="u-script" type="text/javascript" src="{{ URL::asset('public/js/jquery.js') }}" defer=""></script>
-    <script class="u-script" type="text/javascript" src="{{ URL::asset('public/js/nicepage.js') }}" defer=""></script>
+    <script class="u-script" type="text/javascript" src="{{ URL::asset('js/jquery.js') }}" defer=""></script>
+    <script class="u-script" type="text/javascript" src="{{ URL::asset('js/nicepage.js') }}" defer=""></script>
 
     <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
     <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
